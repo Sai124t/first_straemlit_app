89469678c9025eddad43d25898a1dbaf9f8086ce
Sai124t <101164466+Sai124t@users.noreply.github.com>
2023-02-04 13:41:09 +0530

    Update streamlit_app.py
    added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
